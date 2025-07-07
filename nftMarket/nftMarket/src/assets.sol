8148250a60e463407834cc53803a18e64a6c37c6
Vivswaan Singh <vivswaan.singh@syvora.com>
2025-07-07 11:30:33 +0530

    feat: corrected errors

--- nftMarket/nftMarket/src/assets.sol
+++ nftMarket/nftMarket/src/assets.sol
@@ -1,61 +1,65 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "src/coins.sol";
-import "src/assets.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "contracts/newERC20.sol";
 
-contract SellTokens is Ownable {
-    uint256 private _nextTokenId;
-    uint256 private _listedNfts;
-    uint256 public listingPrice=0.0025 ether;
+interface IAssets {
+    error PriceNotSet();
+    error NotOwner();
 
-    mapping(uint256=>mapping(address=>MarketItem)) idToMarketItem;
-    mapping(address => uint256) private sellerEarnings;
+    event Price(uint256 tokenID, uint256 price);
 
-    struct MarketItem { 
-        uint256 tokenID;
-        address sellerAdderess;
-        address nftContractaddress;
-        address paymentAddress;
-        uint256 price;
-        bool active;
-    }
+    function mintAssets(uint256 tokenID) external;
 
-    constructor() Ownable(msg.sender) {}
- 
-    function listNft(address _nftContractAddress,address _ERC20address,uint256 _tokenID, uint256 _price) public payable {
-        require(msg.value<listingPrice,"Insufficient Balance");
-        require(IERC721(_nftContractAddress).ownerOf(_tokenID)==msg.sender, "You are not ownder of this NFT!!!");
-        //IERC721(_nftContractAddress).transferFrom(msg.sender,address(this),_tokenID);
-        _listedNfts++;
-        idToMarketItem[_tokenID][_nftContractAddress]=MarketItem(_tokenID,msg.sender,_nftContractAddress,_ERC20address,_price,true); 
-        if(msg.value>listingPrice){
-            uint256 remainingAmount=msg.value-listingPrice;
-            (bool sent,)=msg.sender.call{value:remainingAmount}("");
-            require(sent,"insufficient Balance");
-        }
-    }
+    function setPrice(uint256 tokenID,uint256 assetCost) external;
 
-    function purchaseNft(address nftAddress, uint256 tokenId) public payable {
-        MarketItem memory listedItem = idToMarketItem[tokenId][nftAddress];
-        require(msg.value < listedItem.price);
-        sellerEarnings[listedItem.sellerAdderess] += msg.value;
-        delete (idToMarketItem[tokenId][nftAddress]);
-        Assets(nftAddress).transferFrom(listedItem.sellerAdderess, msg.sender, tokenId);
-        Coins(listedItem.paymentAddress).assetPurchase(msg.sender,listedItem.sellerAdderess,listedItem.price);
-    }
+    function getPrice(uint256 tokenID) view external returns (uint256);
 
-    function withdraw() public onlyOwner() {
-        uint256 proceeds = sellerEarnings[msg.sender];
-        require(proceeds > 0,"No profits to withdraw");
-
-        (bool success, ) = payable(msg.sender).call{value: proceeds}("");
-        require(success, "Transfer failed");   
-    }
-
+    function buyAsset(uint256 tokenID) external;
 
 }
+
+contract Assets is ERC721, IAssets { 
+    Coins private immutable _coins; // make this immutable
+    mapping (uint256 => uint256) private _assetPrices; // update var name    
+
+    modifier onlyOwner(address tokenOwner) {
+        require(msg.sender == tokenOwner, NotOwner());
+        _;
+    }
+
+    constructor(address coinAddress) ERC721("Asset", "AS") {
+        _coins=Coins(coinAddress);
+    }
+
+    function mintAssets(uint256 tokenID) public {
+        _mint(msg.sender, tokenID);
+    }
+
+    function setPrice(uint256 tokenID,uint256 assetCost) public onlyOwner(ownerOf(tokenID)) {
+        _assetPrices[tokenID] = assetCost;
+        emit Price(tokenID,assetCost);
+    }
+
+// make this external (why)
+    function getPrice(uint256 tokenID) view external returns (uint256) {
+        return _assetPrices[tokenID];
+    }
+
+// external
+    function buyAsset(uint256 tokenID) external {
+        address owner=ownerOf(tokenID);
+        uint256 cost=_assetPrices[tokenID];
+
+        if(cost==0){
+            revert PriceNotSet(); // move custom error to interface
+        }
+
+        _coins.assetPurchase(msg.sender, owner, cost);
+        
+        _transfer(owner, msg.sender, tokenID);
+
+    }
+} 
